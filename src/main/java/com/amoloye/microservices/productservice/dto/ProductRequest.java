9c4b5807302df3b22376160266d07c26cacf51bd
Adedamola Ayodeji Adegoke <51445164+amoloye@users.noreply.github.com>
2025-01-21 18:05:40 +0200

    Delete src directory

--- src/main/java/com/amoloye/microservices/productservice/dto/ProductRequest.java
+++
@@ -1,7 +0,0 @@
-package com.amoloye.microservices.productservice.dto;
-
-import java.math.BigDecimal;
-
-public record ProductRequest(String id, String name, String description, BigDecimal price) {
-
-}
--- src/main/java/com/amoloye/microservices/productservice/dto/ProductResponse.java
+++
@@ -1,7 +0,0 @@
-package com.amoloye.microservices.productservice.dto;
-
-import java.math.BigDecimal;
-
-public record ProductResponse(String id, String name, String description, BigDecimal price) {
-
-}
