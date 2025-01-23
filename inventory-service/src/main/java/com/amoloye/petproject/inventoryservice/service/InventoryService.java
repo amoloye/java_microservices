f9ba934c9fbf9b9072c2256c0eee41e82c9daf17
amoloye <adedamolaadegoke2@gmail.com>
2025-01-23 11:14:24 +0200

    added inventory_service with method to check if inventory isInStock parsing parameters skuCode and quantity. database migration with flyway into mysql database and using docker-compose to create docker container. written integration test using testcontainer and restAssured.

---
+++ inventory-service/src/main/java/com/amoloye/petproject/inventoryservice/service/InventoryService.java
@@ -0,0 +1,20 @@
+package com.amoloye.petproject.inventoryservice.service;
+
+
+import com.amoloye.petproject.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public boolean isInStock(String skuCode, Integer quantity){
+        //Find an inventory for a given skuCode where quantity >= 0
+       return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+
+    }
+
+}
