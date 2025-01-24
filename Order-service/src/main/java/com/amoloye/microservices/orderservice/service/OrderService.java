d30469cdbf2349d52ab62910bdb81597c1fe8947
amoloye <adedamolaadegoke2@gmail.com>
2025-01-24 10:49:41 +0200

    implementing inter-service communication between order-service and inventory-service using the methos isInStock using feignClient. implemented test using wiremock and injecting the dependencies in the pom.xml file. also configured test application.properties file to use wiremock.server.port

--- Order-service/src/main/java/com/amoloye/microservices/orderservice/service/OrderService.java
+++ Order-service/src/main/java/com/amoloye/microservices/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.amoloye.microservices.orderservice.service;
 
+import com.amoloye.microservices.orderservice.client.InventoryClient;
 import com.amoloye.microservices.orderservice.dto.OrderRequest;
 import com.amoloye.microservices.orderservice.model.Order;
 import com.amoloye.microservices.orderservice.repository.OrderRepository;
@@ -12,15 +13,23 @@ import java.util.UUID;
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+      var isProductInStock=  inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+      if (isProductInStock) {
+          Order order = new Order();
+          order.setOrderNumber(UUID.randomUUID().toString());
+          order.setPrice(orderRequest.price());
+          order.setSkuCode(orderRequest.skuCode());
+          order.setQuantity(orderRequest.quantity());
+
+          orderRepository.save(order);
+      }else{
+          throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+      }
+
 
 
 
